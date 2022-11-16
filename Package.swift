b2b39fafe02e9af7c1ade139aea35dfad8a9e0fb
Vijay Sharma <vijay.sharm@gmail.com>
2022-11-16 16:55:07 -0500

    Update dependencies built with Xcode 13.1

--- Package.swift
+++ Package.swift
@@ -39,17 +39,17 @@ let package = Package(
         .binaryTarget(
             name: "TrueTime",
             url: "https://s3.amazonaws.com/swift-package-manager/TrueTime/5.1.0/TrueTime.5.1.0.xcframework.zip",
-            checksum: "07b957755b028bfc325645bff7c75fd1bfdc548e6715cae6d3921145e11a10b3"
+            checksum: "ebf7bc912f9696a5594bbf897bf383ad6b7dc2594e013bc9a086074f61d40e91"
         ),
         .binaryTarget(
             name: "MQTTClient",
             url: "https://s3.amazonaws.com/swift-package-manager/MQTTClient/0.15.3/MQTTClient.0.15.3.xcframework.zip",
-            checksum: "933cb46fc849b595256ac36e0884459bf0516bc62fcf095c4f780042f8579393"
+            checksum: "24bf669606da75a5b7d5cb1f1356939584348139e8c76f031d9fd60d44a0fe3d"
         ),
         .binaryTarget(
             name: "SocketRocket",
             url: "https://s3.amazonaws.com/swift-package-manager/SocketRocket/0.5.1/SocketRocket.0.5.1.xcframework.zip",
-            checksum: "832105e93a00508e068d324645fbd3966eb9f28ab4d909f87686e9e432be517c"
+            checksum: "6995aefb57060faee49ff69bb58042bd84b5c575fe116a2707558aabe5c6cd9c"
         )
     ]
 )
